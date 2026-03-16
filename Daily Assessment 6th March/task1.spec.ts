e9aa54068113d804330ee7c68ea1847011a8f67b
keshavk21 <khandelwalkeshav2004@gmail.com>
2026-03-16 12:14:39 +0530

    work till day 19

--- Daily Assessment 6th March/task1.spec.ts
+++
@@ -1,7 +0,0 @@
-import {test} from '@playwright/test';
-test('Task 1', async ({page}) => {
-    await page.goto('https://www.olympics.com/en/olympic-games/tokyo-2020');
-    await page.locator("//a[text()='All Athletes']").click();
-    const medalCount=await page.locator("//li[@data-row-id='all-athletes-table-row-2']/descendant::span[@data-cy='ocs-text-module']").first().textContent();
-    console.log("Medal Count: " + medalCount);
-})
